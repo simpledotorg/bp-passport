9405718a77eecbffeef44b47b03c919845606a32
Nick Kuh <nick.kuh@matchboxmobile.com>
2020-04-09 08:36:39 +0100

    Stricter typing added to components Cleaned up some layout code in Splash screen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,46 +1,30 @@
 import React from 'react'
-import {SafeAreaView, View, Text, Image} from 'react-native'
+import {SafeAreaView, View, Image, StyleSheet} from 'react-native'
 import {FormattedMessage} from 'react-intl'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import {containerStyles, colors, iconSplash, bpLogo} from '../styles'
-import {Button, PageHeader, BodyHeader, BodyText} from '../components'
+import {Button, BodyHeader, BodyText} from '../components'
 import SCREENS from '../constants/screens'
 
 function SplashScreen({navigation}: any) {
   return (
     <SafeAreaView
       style={[containerStyles.fill, {backgroundColor: colors.white}]}>
-      <View
-        style={[
-          containerStyles.fill,
-          containerStyles.centeredContent,
-          {margin: 24, justifyContent: 'flex-start', alignItems: 'flex-start'},
-        ]}>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            paddingTop: 24,
-            paddingBottom: 24,
-            width: '100%',
-            borderBottomColor: colors.grey3,
-            borderBottomWidth: 1,
-            marginBottom: 24,
-          }}>
-          <Image
-            source={iconSplash}
-            style={{width: 32, height: 32, flexShrink: 0, marginRight: 14}}
-          />
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <Image source={iconSplash} style={{marginRight: 14}} />
           <Image
             source={bpLogo}
-            style={{height: 32, resizeMode: 'contain', marginTop: 5}}
+            style={{height: 20, resizeMode: 'contain', marginTop: 5}}
           />
         </View>
+
         <BodyHeader>
           <FormattedMessage id="splash.sub-title" />
         </BodyHeader>
-        <View style={{width: '90%'}}>
+
+        <View>
           <View
             style={{
               marginTop: 24,
@@ -48,46 +32,31 @@ function SplashScreen({navigation}: any) {
               alignItems: 'center',
             }}>
             <Icon name="edit" size={24} />
-            <BodyText style={{marginLeft: 16}}>
+            <BodyText style={styles.itemText}>
               <FormattedMessage id="splash.track" />
             </BodyText>
           </View>
-          <View
-            style={{
-              marginTop: 24,
-              flexDirection: 'row',
-              alignItems: 'center',
-            }}>
+          <View style={styles.item}>
             <Icon name="record-voice-over" size={24} />
-            <BodyText style={{marginLeft: 16}}>
+            <BodyText style={styles.itemText}>
               <FormattedMessage id="splash.talk" />
             </BodyText>
           </View>
-          <View
-            style={{
-              marginTop: 24,
-              flexDirection: 'row',
-              alignItems: 'center',
-            }}>
+          <View style={styles.item}>
             <Icon name="local-pharmacy" size={24} />
-            <BodyText style={{marginLeft: 16}}>
+            <BodyText style={styles.itemText}>
               <FormattedMessage id="splash.medicine" />
             </BodyText>
           </View>
-          <View
-            style={{
-              marginTop: 24,
-              flexDirection: 'row',
-              alignItems: 'center',
-            }}>
+          <View style={styles.item}>
             <Icon name="alarm" size={24} />
-            <BodyText style={{marginLeft: 16}}>
+            <BodyText style={styles.itemText}>
               <FormattedMessage id="splash.reminders" />
             </BodyText>
           </View>
         </View>
         <Button
-          style={{width: '100%', marginTop: 24}}
+          style={{marginTop: 24}}
           title={<FormattedMessage id="general.next" />}
           onPress={() => {
             navigation.navigate(SCREENS.CONSENT)
@@ -99,3 +68,17 @@ function SplashScreen({navigation}: any) {
 }
 
 export default SplashScreen
+
+const styles = StyleSheet.create({
+  content: {margin: 24},
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 24,
+    borderBottomColor: colors.grey3,
+    borderBottomWidth: 1,
+    marginBottom: 24,
+  },
+  item: {marginTop: 24, flexDirection: 'row', alignItems: 'center'},
+  itemText: {marginLeft: 16, flex: 1},
+})
--- src/styles/images.ts
+++ src/styles/images.ts
@@ -1,7 +1,8 @@
-const iconSplash = require('../assets/images/icon-splash.png')
+const iconLaunch = require('../assets/images/icon-launch.png')
 const bpPassportImage = require('../assets/images/bp-passport.png')
 const qrImage = require('../assets/images/qr-code.png')
 const qrMaskImage = require('../assets/images/qr-mask.png')
-const bpLogo = require('../assets/images/logo.png')
+const iconSplash = require('../assets/images/icon-splash.png')
+const bpLogo = require('../assets/images/logo-splash.png')
 
-export {iconSplash, bpPassportImage, bpLogo, qrImage, qrMaskImage}
+export {iconLaunch, bpPassportImage, iconSplash, bpLogo, qrImage, qrMaskImage}
