bbb3d610a9c7eb6c6b2770b1172424d327604479
kenjam97 <jam.ken1997@gmail.com>
2020-04-16 14:45:20 +0100

    fix all broken translation keys, copy for hindi, marathi, punjabi

--- src/components/home-header.tsx
+++ src/components/home-header.tsx
@@ -24,8 +24,7 @@ export const HomeHeaderTitle = () => {
       )}
       {hasPasswordDigest && (
         <Text style={{...navigation.homeSubHeaderTitleStyle}} numberOfLines={1}>
-          <FormattedMessage id="home.password-digest-prefix" />{' '}
-          {user?.password_digest}
+          <FormattedMessage id="general.bp-passport" /> {user?.password_digest}
         </Text>
       )}
     </View>
