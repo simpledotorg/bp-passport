2e3d63180a7a9897c5bb5d74f75226f1f29615e0
Nick Kuh <nick.kuh@matchboxmobile.com>
2020-04-16 11:30:04 +0100

    Rebuild in app center

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-cp -f ./.env.production.default ./.env.production
+case $APPCENTER_BRANCH in
+    "staging")
+        cp -f ./.env.production.default ./.env.production
+        ;;
+    "test")
+        cp -f ./.env.production.default ./.env.production
+        ;;
+    *)
+        cp -f ./.env.production.default ./.env.production
+        ;;
+esac
