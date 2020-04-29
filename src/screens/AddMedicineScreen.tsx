44191255c446900762fb06878b1a5901eaac6502
Sam Richards <samuelmarkrichards@gmail.com>
2020-04-29 11:52:59 +0100

    fixed flat lists and non-scrolling text field

--- src/screens/AddMedicineScreen.tsx
+++ src/screens/AddMedicineScreen.tsx
@@ -6,6 +6,7 @@ import {
   TextInput,
   TouchableWithoutFeedback,
   Image,
+  FlatList,
 } from 'react-native'
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
 import {RouteProp} from '@react-navigation/native'
@@ -40,8 +41,30 @@ function AddMedicineScreen({navigation, route}: Props) {
   const inputRef = useRef<null | any>(null)
 
   return (
-    <KeyboardAwareScrollView style={{flex: 1, backgroundColor: colors.grey4}}>
-      <SafeAreaView style={[containerStyles.fill]}>
+    <SafeAreaView style={[containerStyles.fill]}>
+      <View
+        style={[
+          styles.card,
+          {
+            padding: 12,
+            backgroundColor: colors.white100,
+            borderBottomWidth: 1,
+            borderColor: colors.grey4,
+          },
+        ]}>
+        <TextInput
+          style={[styles.input]}
+          ref={inputRef}
+          autoFocus
+          placeholder={intl.formatMessage({
+            id: 'medicine.enter-name',
+          })}
+          value={input}
+          onChangeText={(text) => setInput(text)}
+          maxLength={6}
+        />
+      </View>
+      <KeyboardAwareScrollView style={{flex: 1, backgroundColor: colors.grey4}}>
         <TouchableWithoutFeedback
           style={{flex: 1}}
           onPress={() => {
@@ -51,69 +74,51 @@ function AddMedicineScreen({navigation, route}: Props) {
           }}>
           <View>
             <View
-              style={[
-                styles.card,
-                {
-                  padding: 12,
-                  backgroundColor: colors.white100,
-                },
-              ]}>
-              <TextInput
-                style={[styles.input]}
-                ref={inputRef}
-                autoFocus
-                placeholder={intl.formatMessage({
-                  id: 'medicine.enter-name',
-                })}
-                value={input}
-                onChangeText={(text) => setInput(text)}
-                maxLength={6}
-              />
-            </View>
-            <View
               style={{
                 paddingHorizontal: 8,
               }}>
-              {MEDICINES.map((medicine: string) => {
-                if (
-                  !medicine.toLowerCase().includes(input.toLowerCase()) ||
-                  input === ''
-                )
-                  return null
+              <FlatList
+                data={MEDICINES}
+                renderItem={({item}) => {
+                  if (
+                    !item.toLowerCase().includes(input.toLowerCase()) ||
+                    input === ''
+                  ) {
+                    return null
+                  }
 
-                return (
-                  <View
-                    key={medicine}
-                    style={[
-                      styles.card,
-                      {
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        marginTop: 8,
-                        backgroundColor: colors.white100,
-                        borderRadius: 4,
-                      },
-                    ]}>
-                    <Image
-                      source={medicinePill}
-                      style={{
-                        marginHorizontal: 8,
-                        flexShrink: 0,
-                        height: 40,
-                        width: 40,
-                      }}
-                    />
-                    <BodyText style={[styles.medicineText]}>
-                      {medicine}
-                    </BodyText>
-                  </View>
-                )
-              })}
+                  return (
+                    <View
+                      key={item}
+                      style={[
+                        styles.card,
+                        {
+                          flexDirection: 'row',
+                          alignItems: 'center',
+                          marginTop: 8,
+                          backgroundColor: colors.white100,
+                          borderRadius: 4,
+                        },
+                      ]}>
+                      <Image
+                        source={medicinePill}
+                        style={{
+                          marginHorizontal: 8,
+                          flexShrink: 0,
+                          height: 40,
+                          width: 40,
+                        }}
+                      />
+                      <BodyText style={[styles.medicineText]}>{item}</BodyText>
+                    </View>
+                  )
+                }}
+              />
             </View>
           </View>
         </TouchableWithoutFeedback>
-      </SafeAreaView>
-    </KeyboardAwareScrollView>
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
   )
 }
 
@@ -143,7 +148,7 @@ const styles = StyleSheet.create({
     color: colors.grey1,
   },
   card: {
-    shadowColor: 'rgba(0,0,0, 0.16)', // iOS box shadow
+    shadowColor: 'rgba(0, 0, 0, 0.16)', // iOS box shadow
     shadowOffset: {height: 1, width: 1}, // iOS box shadow
     shadowOpacity: 1, // iOS box shadow
     shadowRadius: 1, // iOS box shadow
