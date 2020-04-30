2f0894a08e308cf1cf33ced60a0683f76e171f87
kenjam97 <jam.ken1997@gmail.com>
2020-04-30 12:49:31 +0100

    finish styling splash screen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -29,13 +29,80 @@ function SplashScreen({navigation}: Props) {
   const intl = useIntl()
 
   return (
+    <View
+      style={[
+        containerStyles.fill,
+        {
+          backgroundColor: colors.grey4,
+          justifyContent: 'flex-end',
+        },
+      ]}>
+      <View
+        style={[
+          styles.splashContainer,
+          {
+            position: 'relative',
+            height: '30%',
+            justifyContent: 'center',
+            flexDirection: 'row',
+          },
+        ]}>
+        <Image
+          source={splashImage}
+          style={{position: 'absolute', bottom: '90%'}}
+        />
+        <View>
+          <PageHeader
+            style={{
+              textAlign: 'center',
+              marginBottom: 18,
+              marginTop: '15%',
+            }}>
+            <FormattedMessage id="splash.track-bp-bs-meds" />
+          </PageHeader>
+
+          <Button
+            style={[styles.primaryButton]}
+            buttonColor={colors.blue2}
+            title={intl.formatMessage({id: 'general.next'})}
+            onPress={() => {
+              navigation.navigate(SCREENS.LOGIN)
+            }}
+          />
+        </View>
+      </View>
+    </View>
+  )
+
+  return (
     <SafeAreaView
       style={[containerStyles.fill, {backgroundColor: colors.grey4}]}>
       <StatusBar barStyle="dark-content" />
       <View style={{flex: 1, justifyContent: 'flex-end'}}>
-        <View style={[styles.splashContainer, {alignItems: 'center'}]}>
-          <Image source={splashImage} style={{}} />
-          <PageHeader style={{textAlign: 'center', marginBottom: 18}}>
+        <View style={{flex: 2, flexGrow: 1, flexShrink: 0}} />
+        <View
+          style={[
+            styles.splashContainer,
+            {
+              flex: 1,
+              alignItems: 'center',
+              position: 'relative',
+              backgroundColor: 'blue',
+            },
+          ]}>
+          {/* <Image
+            source={splashImage}
+            style={{position: 'absolute', bottom: '100%'}}
+          /> */}
+          <PageHeader
+            style={{
+              textAlign: 'center',
+              marginBottom: 18,
+              marginTop: '20%',
+              position: 'absolute',
+              bottom: '100%',
+              backgroundColor: 'red',
+            }}>
             <FormattedMessage id="splash.track-bp-bs-meds" />
           </PageHeader>
 
@@ -59,8 +126,7 @@ const styles = {
   splashContainer: {
     backgroundColor: colors.white100,
     borderRadius: 4,
-    flexShrink: 0,
-    padding: 24,
+    paddingHorizontal: 24,
 
     shadowColor: colors.black,
     shadowOffset: {
