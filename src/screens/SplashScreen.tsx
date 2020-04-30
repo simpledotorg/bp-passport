029fcc804b3a9f67d4afc026c816fe9a251afaa3
kenjam97 <jam.ken1997@gmail.com>
2020-04-30 12:50:19 +0100

    remove unused return

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -73,51 +73,6 @@ function SplashScreen({navigation}: Props) {
       </View>
     </View>
   )
-
-  return (
-    <SafeAreaView
-      style={[containerStyles.fill, {backgroundColor: colors.grey4}]}>
-      <StatusBar barStyle="dark-content" />
-      <View style={{flex: 1, justifyContent: 'flex-end'}}>
-        <View style={{flex: 2, flexGrow: 1, flexShrink: 0}} />
-        <View
-          style={[
-            styles.splashContainer,
-            {
-              flex: 1,
-              alignItems: 'center',
-              position: 'relative',
-              backgroundColor: 'blue',
-            },
-          ]}>
-          {/* <Image
-            source={splashImage}
-            style={{position: 'absolute', bottom: '100%'}}
-          /> */}
-          <PageHeader
-            style={{
-              textAlign: 'center',
-              marginBottom: 18,
-              marginTop: '20%',
-              position: 'absolute',
-              bottom: '100%',
-              backgroundColor: 'red',
-            }}>
-            <FormattedMessage id="splash.track-bp-bs-meds" />
-          </PageHeader>
-
-          <Button
-            style={[styles.primaryButton]}
-            buttonColor={colors.blue2}
-            title={intl.formatMessage({id: 'general.next'})}
-            onPress={() => {
-              navigation.navigate(SCREENS.LOGIN)
-            }}
-          />
-        </View>
-      </View>
-    </SafeAreaView>
-  )
 }
 
 export default SplashScreen
