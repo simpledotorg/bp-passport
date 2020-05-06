3c82a624faeb4d0cc7128db2c54bb858af5f9944
kenjam97 <jam.ken1997@gmail.com>
2020-05-06 14:01:10 +0100

    increase horizontal margin

--- src/styles/containers.ts
+++ src/styles/containers.ts
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   containerSegment: {
     backgroundColor: colors.white100,
     borderRadius: 4,
-    marginHorizontal: 8,
+    marginHorizontal: 18,
     marginBottom: 8,
     flexShrink: 0,
     padding: 24,
