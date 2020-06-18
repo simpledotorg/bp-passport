1e1a0e84d45e6dfd9661bd3891f464cb9c034caf
kenjam97 <jam.ken1997@gmail.com>
2020-06-18 09:50:57 +0100

    implement tooltips and highlight data point correctly

--- src/components/bs-history-chart.tsx
+++ src/components/bs-history-chart.tsx
@@ -119,7 +119,14 @@ export const BsHistoryChart = ({bss}: Props) => {
 
   const generateScatter = (bss: DateRange[]): any[] => {
     return bss.map((bs: DateRange) => {
-      return {x: bs.index, y: bs.averaged, label: bs.averaged}
+      return {
+        x: bs.index,
+        y: bs.averaged,
+        label: `${bs.averaged.toFixed(2)}, ${format(
+          bs.date,
+          'dd-MM-yyyy',
+        )}, ${format(bs.date, 'k:mm')}`,
+      }
     })
   }
 
@@ -397,7 +404,58 @@ export const BsHistoryChart = ({bss}: Props) => {
                 fill: colors.green1,
               },
             }}
-            labelComponent={<VictoryTooltip renderInPortal={false} />}
+            events={[
+              {
+                target: 'data',
+                eventHandlers: {
+                  onPressIn: () => {
+                    return [
+                      {
+                        target: 'data',
+                        mutation: () => ({
+                          style: {
+                            stroke: colors.blue2,
+                            strokeWidth: 3,
+                            fill: colors.white,
+                            boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)',
+                          },
+                        }),
+                      },
+                      {
+                        target: 'labels',
+                        mutation: () => ({active: true}),
+                      },
+                    ]
+                  },
+                  onPressOut: () => {
+                    return [
+                      {
+                        target: 'data',
+                        mutation: () => {},
+                      },
+                      {
+                        target: 'labels',
+                        mutation: () => ({active: false}),
+                      },
+                    ]
+                  },
+                },
+              },
+            ]}
+            labelComponent={
+              <VictoryTooltip
+                renderInPortal={true}
+                constrainToVisibleArea={true}
+                cornerRadius={20}
+                pointerLength={5}
+                flyoutStyle={{
+                  height: 32,
+                  padding: 200,
+                  fill: colors.grey0,
+                }}
+                style={{fill: colors.white}}
+              />
+            }
           />
           <VictoryScatter
             data={generateScatter(chartData.high)}
@@ -407,7 +465,58 @@ export const BsHistoryChart = ({bss}: Props) => {
                 fill: colors.red1,
               },
             }}
-            labelComponent={<VictoryTooltip renderInPortal={false} />}
+            events={[
+              {
+                target: 'data',
+                eventHandlers: {
+                  onPressIn: () => {
+                    return [
+                      {
+                        target: 'data',
+                        mutation: () => ({
+                          style: {
+                            stroke: colors.blue2,
+                            strokeWidth: 3,
+                            fill: colors.white,
+                            boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)',
+                          },
+                        }),
+                      },
+                      {
+                        target: 'labels',
+                        mutation: () => ({active: true}),
+                      },
+                    ]
+                  },
+                  onPressOut: () => {
+                    return [
+                      {
+                        target: 'data',
+                        mutation: () => {},
+                      },
+                      {
+                        target: 'labels',
+                        mutation: () => ({active: false}),
+                      },
+                    ]
+                  },
+                },
+              },
+            ]}
+            labelComponent={
+              <VictoryTooltip
+                renderInPortal={true}
+                constrainToVisibleArea={true}
+                cornerRadius={20}
+                pointerLength={5}
+                flyoutStyle={{
+                  height: 32,
+                  padding: 200,
+                  fill: colors.grey0,
+                }}
+                style={{fill: colors.white}}
+              />
+            }
           />
         </VictoryChart>
       </View>
