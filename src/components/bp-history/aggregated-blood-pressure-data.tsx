a92f77065951f33b8aa7f5bf6a027d0f7a017a6c
kenjam97 <jam.ken1997@gmail.com>
2020-06-24 11:50:28 +0100

    remove commented code and add aggregated data file

---
+++ src/components/bp-history/aggregated-blood-pressure-data.tsx
@@ -0,0 +1,64 @@
+import {DateEntry} from './date-entry'
+import {BloodPressure} from '../../redux/blood-pressure/blood-pressure.models'
+
+export class AggregatedBloodPressureData {
+  private dateEntry: DateEntry
+
+  private minReading: BloodPressure | null = null
+  private maxReading: BloodPressure | null = null
+
+  private readings: BloodPressure[] = []
+
+  public constructor(dateEntry: DateEntry) {
+    this.dateEntry = dateEntry
+  }
+
+  public getDateEntry(): DateEntry {
+    return this.dateEntry
+  }
+
+  private updateMinReading(reading: BloodPressure): void {
+    if (!this.minReading) {
+      this.minReading = reading
+    }
+
+    const readingValue = Number(reading.blood_sugar_value)
+    const currentMinValue = Number(this.minReading.blood_sugar_value)
+
+    if (currentMinValue > readingValue) {
+      this.minReading = reading
+    }
+  }
+
+  private updateMaxReading(reading: BloodPressure): void {
+    if (!this.maxReading) {
+      this.maxReading = reading
+      return
+    }
+
+    const readingValue = Number(reading.blood_sugar_value)
+    const currentMaxValue = Number(this.maxReading.blood_sugar_value)
+
+    if (readingValue > currentMaxValue) {
+      this.maxReading = reading
+    }
+  }
+
+  public addReading(reading: BloodPressure): void {
+    this.readings.push(reading)
+    this.updateMinReading(reading)
+    this.updateMaxReading(reading)
+  }
+
+  public getReadings(): BloodPressure[] {
+    return this.readings
+  }
+
+  public getMaxReading(): BloodPressure | null {
+    return this.maxReading
+  }
+
+  public getMinReading(): BloodPressure | null {
+    return this.minReading
+  }
+}
