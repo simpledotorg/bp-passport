a012baf817508f363e06f0388991fa32a4f5ec44
Paul Hadfield <paul@automationsquared.com>
2020-07-01 21:03:29 +0100

    Rendering speed

--- src/screens/BsHistoryScreen.tsx
+++ src/screens/BsHistoryScreen.tsx
@@ -1,16 +1,18 @@
 import React, {useEffect, useState} from 'react'
-import {View, StyleSheet, TouchableHighlight, ScrollView} from 'react-native'
+import {View, ScrollView} from 'react-native'
 import {RouteProp} from '@react-navigation/native'
 import {StackNavigationProp} from '@react-navigation/stack'
 import {FormattedMessage} from 'react-intl'
 
-import {containerStyles, colors} from '../styles'
-import {BodyHeader, BsInformation, BsHistoryChart, Line} from '../components'
+import {containerStyles} from '../styles'
+import {BodyHeader, BsHistoryChart} from '../components'
 import SCREENS from '../constants/screens'
 import {RootStackParamList} from '../Navigation'
 import {bloodSugarsSelector} from '../redux/blood-sugar/blood-sugar.selectors'
 import {bloodSugarUnitSelector} from '../redux/patient/patient.selectors'
 import ConvertedBloodSugarReading from '../models/converted_blood_sugar_reading'
+import HistoryList from '../components/bs-history/history-list'
+
 import {getTestData} from '../components/bs-history/test-data'
 
 type BsHistoryScreenNavigationProp = StackNavigationProp<
@@ -75,61 +77,14 @@ function BsHistoryScreen({navigation, route}: Props) {
             />
           </View>
         </View>
-        <View
-          style={[
-            containerStyles.containerSegment,
-            {paddingVertical: 22, paddingHorizontal: 24},
-          ]}>
-          <View style={[{flexShrink: 0}]}>
-            <BodyHeader
-              style={{fontSize: 22, fontWeight: 'bold', marginBottom: 14}}>
-              <FormattedMessage id="page-titles.all-bs" />
-            </BodyHeader>
-            <Line />
-            <View>
-              {convertedReadings?.map((bs, index) => (
-                <View key={index}>
-                  <TouchableHighlight
-                    underlayColor={colors.grey4}
-                    activeOpacity={0.8}
-                    onPress={() => {
-                      navigation.navigate(SCREENS.DETAILS_MODAL_SCREEN, {
-                        bs,
-                      })
-                    }}
-                    key={index}
-                    style={[
-                      {
-                        paddingVertical: 12,
-                        marginHorizontal: -24,
-                        paddingHorizontal: 24,
-                      },
-                      styles.historyItem,
-                      index === convertedReadings.length - 1
-                        ? {borderBottomWidth: 0}
-                        : {},
-                    ]}>
-                    <BsInformation bs={bs} displayUnits={displayUnits} />
-                  </TouchableHighlight>
-                  {index < convertedReadings.length - 1 && (
-                    <Line key={'line' + index} />
-                  )}
-                </View>
-              ))}
-            </View>
-          </View>
-        </View>
+        <HistoryList
+          convertedReadings={convertedReadings}
+          navigation={navigation}
+          displayUnits={displayUnits}
+        />
       </ScrollView>
     </View>
   )
 }
 
 export default BsHistoryScreen
-
-const styles = StyleSheet.create({
-  historyItem: {
-    paddingBottom: 12,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-})
